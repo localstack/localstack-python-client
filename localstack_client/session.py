58b19f61f582c29818aa7ebfde9c2aae0f942d0e
Joe Pohlmann <73117648+josephpohlmann@users.noreply.github.com>
2021-08-30 06:20:31 -0400

    add ability to specify region and kwargs when creating boto3 client sessions (#31)

--- localstack_client/session.py
+++ localstack_client/session.py
@@ -82,8 +82,12 @@ def _get_default_session():
 
 
 def client(*args, **kwargs):
+    if kwargs:
+        return Session(**kwargs).client(*args, **kwargs)
     return _get_default_session().client(*args, **kwargs)
 
 
 def resource(*args, **kwargs):
+    if kwargs:
+        return Session(**kwargs).resource(*args, **kwargs)
     return _get_default_session().resource(*args, **kwargs)
