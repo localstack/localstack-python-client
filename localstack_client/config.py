08eb3e66fbac3bc35bda4cbd824efc19ff5b47a5
Waldemar Hummer <waldemar.hummer@gmail.com>
2021-10-13 18:51:49 +0200

    add endpoint/alias for configservice

--- localstack_client/config.py
+++ localstack_client/config.py
@@ -101,6 +101,7 @@ _service_endpoints_template = {
     'waf': '{proto}://{host}:4640',
     'wafv2': '{proto}://{host}:4640',
     'config': '{proto}://{host}:4641',
+    'configservice': '{proto}://{host}:4641',
 }
 
 # TODO remove service port mapping above entirely
